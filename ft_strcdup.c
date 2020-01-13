52a52f3b4305e791258c57ea759085e038da1090
Cristina Jover navarro <cjover-n@c2r19s3.42madrid.com>
2020-01-13 13:17:22 +0100

    funcion cambiada por una sin otros auxiliares raros

--- ft_strcdup.c
+++ ft_strcdup.c
@@ -6,24 +6,26 @@
 /*   By: cjover-n <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/13 11:54:13 by cjover-n          #+#    #+#             */
-/*   Updated: 2020/01/13 11:54:27 by cjover-n         ###   ########.fr       */
+/*   Updated: 2020/01/13 13:16:39 by cjover-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strcdup(const char *string, char delim)
+char    *ft_strcdup(const char *s1, int c)
 {
-	char	*ret;
-	size_t	index;
-	index = 0;
-	ret = (char *)malloc(sizeof(char) * (ft_strlenc(string, delim) + 1));
-	if (ret != NULL)
-	{
-		while (string[index] != delim)
-		{
-			ret[index] = string[index];
-			index++;
-		}
-		ret[index] = '\0';
-	}
-	return (ret);
+    int     i;
+    char    *str;
+    char    car;
+    i = 0;
+    car = (char)c;
+    while (s1[i] != car && s1[i] != '\0')
+        i++;
+    str = malloc(i + 1 * sizeof(char));
+    str[i] = '\0';
+    if (str == NULL)
+    {
+        return (NULL);
+    }
+    while (i-- > 0)
+        str[i] = s1[i];
+    return (str);
 }
