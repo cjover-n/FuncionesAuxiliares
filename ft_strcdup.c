44f82d91c6cc86683799aa7466e998420ab4ac07
cjover-n <cristinajover93@gmail.com>
2020-01-17 12:47:35 +0100

    función corregida
    malloc protegido y reducidas las variables

--- ft_strcdup.c
+++ ft_strcdup.c
@@ -14,17 +14,13 @@ char    *ft_strcdup(const char *s1, int c)
 {
     int     i;
     char    *str;
-    char    car;
+
     i = 0;
-    car = (char)c;
-    while (s1[i] != car && s1[i] != '\0')
+    while (s1[i] != (char)c && s1[i] != '\0')
         i++;
-    str = malloc(i + 1 * sizeof(char));
-    str[i] = '\0';
-    if (str == NULL)
-    {
+    if (!(str = malloc(i + 1 * sizeof(char))))
         return (NULL);
-    }
+    str[i] = '\0';
     while (i-- > 0)
         str[i] = s1[i];
     return (str);
